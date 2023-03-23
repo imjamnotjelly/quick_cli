07500eb7429715ffa9ff2ac0396f0e4183be8da0
imjamnotjelly <jonahmitre@gmail.com>
2023-03-23 10:18:44 -0400

    add select menu

--- functions.py
+++ functions.py
@@ -1,20 +1,39 @@
 import quick_styles as qs
 import keyboard as kb
 
-options = ["HELLO", "THERE", "WORLD"]
-selected_index = 0
 
-while True:
-    for c, v in enumerate(options):
-        if c == selected_index:
-            qs.cprint(v, color="white", bgcolor="blue", styles="bold")
-        else:
-            print(v)
-    key = kb.read_key(suppress=True)
-    match key:
-        case "up":
-            if selected_index > 0:
-                selected_index -= 1
-        case "down":
-            if selected_index < len(options)-1: 
-                selected_index += 1
+def filtered_key():
+    keys = []
+    while len(keys) < 2:
+        keys.append(kb.read_key())
+    return keys[0]
+
+def clear_lines(amt):
+    print("\033[F\033[K"*amt, end="")
+
+def select_menu(options):
+    selected_index = 0
+    last_index = len(options)-1
+    while True:
+        for c, v in enumerate(options):
+            if c == selected_index:
+                qs.cprint(v, color="white", bgcolor="blue", styles="bold")
+            else:
+                print(v)
+
+        key = filtered_key()
+        clear_lines(3)
+        match key:
+            case "up":
+                if selected_index != 0:
+                    selected_index -= 1
+                else:
+                    selected_index = last_index
+            case "down":
+                if selected_index != last_index: 
+                    selected_index += 1
+                else:
+                    selected_index = 0
+            case "enter":
+                print("\033[K", end="")
+                return options[selected_index]
